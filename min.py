28be16a090a09a347a91c946fc3feedf98eccc02
RavikantiShreya <117636459+RavikantiShreya@users.noreply.github.com>
2022-11-07 15:51:00 +0530

    Update min.py

--- min.py
+++ min.py
@@ -1,9 +1,5 @@
 l= [8, 1, 7, 10, 5]
-min, max = l[0], l[0]
-for i in range(1, len(l)):
-	if l[i] < min:
-		min = l[i]
-	if l[i] > max:
-		max=l[i]
-print('Minimum Element in the list', l, 'is', min)
-print('Maximum Element in the list', l, 'is', max)
+print('Minimum Element in the list', min(l))
+print('Maximum Element in the list', max(l))
+print('Maximum Element in the list', avg(l))
+
